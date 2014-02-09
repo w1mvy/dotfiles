97a97be2d7d7d6ebca9ba3f4e41e2c91ae946af0
vagrant <vagrant@precise64.(none)>
2014-02-09 08:14:38 +0000

    update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,9 @@ git submodule init
 git submodule update
 
 # setting vim
+if [ ! -d ~/.vim/bundle ]; then mkdir -p ~/.vim/bundle ;fi
 git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 vim +BundleInstall +qall
 ln -s $HOME/.vim/bundle/vimproc/autoload/* $HOME/.vim/autoload
 ln -s $HOME/.vim/bundle/vimproc/plugin/* $HOME/.vim/plugin
+source ~/.zshrc
